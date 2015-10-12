35da88decfbbbada72148714e55449ee1a6f082d
zhy2002 <hz141@uowmail.edu.au>
2015-10-12 15:45:32 +1100

    Added configuration value for the first provider.

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -3,24 +3,36 @@
 
     var app = angular.module("app", []);
 
-    app.config(["$provide", configApp]);
+    app.provider("books", booksProvider);
 
-    function configApp($provide){
+    //This constructs the book provider
+    function booksProvider(){
 
-        $provide.provider("books", booksProvider);
+        //configuration values; set in configuration phase and used when $get is called.
+        var includeVersionInAppName = false;
+        this.setIncludeVersionInAppName = function(val){
+            includeVersionInAppName = val;
+        };
 
-        function booksProvider(){
+        //the function used to create the service
+        this.$get = function(){
 
-            this.$get = function(){
+            var appName = "Book Logger";
+            var appVersion = "1.0";
+            var appDesc = "Track which books you read.";
 
-                return {
-                    appName : "Book Logger",
-                    appDesc : "Track which books you read."
-                };
+            return {
+                appName : appName + (includeVersionInAppName ? " " + appVersion: ""),
+                appDesc : appDesc
             };
+        };
 
-        }
+    }
 
+    app.config(["booksProvider", configApp]);
+
+    function configApp(booksProvider){
+        booksProvider.setIncludeVersionInAppName(true);
 
     }
 }());
