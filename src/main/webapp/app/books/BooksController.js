f167793a74941ff0d86f909d92a6e64a3bfed96f
zhy2002 <hz141@uowmail.edu.au>
2015-10-14 00:05:37 +1100

    using the $q service.

--- src/main/webapp/app/books/BooksController.js
+++ src/main/webapp/app/books/BooksController.js
@@ -9,9 +9,39 @@
 
         vm.appName  = books.appName;
         vm.appDesc = books.appDesc;
+        vm.allBooks = [];
 
-        vm.allBooks = dataService.getAllBooks();
-        vm.allReaders = dataService.getAllReaders();
+        dataService.getAllBooks().then(getBooksSuccess, getBooksError, getBooksNotification);
+
+        function getBooksSuccess(array){
+            vm.allBooks = array;
+        }
+
+        function getBooksError(errorMessage){
+            logger.output(errorMessage);
+        }
+
+        function getBooksNotification(message){
+            logger.output(message);
+        }
+
+        dataService.getAllReaders().then(getReadersSuccess).catch(getReadersError).finally(getAllReadersComplete);;//catch handler can also handle exceptions thrown from the success handler.
+
+        function getReadersSuccess(array) {
+            if(Math.random() * 100 >= 50) {
+                vm.allReaders = array;
+            } else {
+                throw "An exception occurred while displaying all readers.";
+            }
+        }
+
+        function getReadersError(errorMessage){
+            alert(errorMessage);
+        }
+
+        function getAllReadersComplete(){ //no arguments passed
+            logger.output("getAllReaders completed.");
+        }
 
         vm.getBadge = function(minutesRead){
             return badgeService.retrieveBadge(minutesRead);
--- src/main/webapp/app/services/dataService.js
+++ src/main/webapp/app/services/dataService.js
@@ -3,9 +3,54 @@
 
     angular.module("app").factory("dataService", dataServiceFactory);
 
-    function dataServiceFactory(logger) {
+    var booksArray = [
+        {
+            book_id: 1,
+            title: "Harry Potter and the Deathly Hallows",
+            author: "J.K Rowling",
+            year_published: 2000
+        },
+        {
+            book_id: 2,
+            title: "The Cat in the Hat",
+            author: "Dr. Seuss",
+            year_published: 1957
+        },
+        {
+            book_id: 3,
+            title: "Encyclopedia Brown, Boy Detective",
+            author: "Donald J. Sobol",
+            year_published: 1963
+        }
+    ];
+
+    var readersArray = [
+        {
+            reader_id: 1,
+            name: "Marie",
+            weeklyReadingGoal: 315,
+            totalMinutesRead: 5600
+        },
+        {
+            reader_id: 2,
+            name: "Daniel",
+            weeklyReadingGoal: 210,
+            totalMinutesRead: 3000
+        },
+        {
+            reader_id: 3,
+            name: "Lanier",
+            weeklyReadingGoal: 140,
+            totalMinutesRead: 600
+        }
+
+    ];
+
+    function dataServiceFactory(logger, $q, $timeout) {
 
         logger.output("Creating the dataService instance.");
+
+        var getAllBooksCount = 0;
 
         return {
             getAllBooks: getAllBooks,
@@ -15,54 +60,33 @@
         function getAllBooks() {
 
             logger.output("Getting all books.");
-            return [
-                {
-                    book_id: 1,
-                    title: "Harry Potter and the Deathly Hallows",
-                    author: "J.K Rowling",
-                    year_published: 2000
-                },
-                {
-                    book_id: 2,
-                    title: "The Cat in the Hat",
-                    author: "Dr. Seuss",
-                    year_published: 1957
-                },
-                {
-                    book_id: 3,
-                    title: "Encyclopedia Brown, Boy Detective",
-                    author: "Donald J. Sobol",
-                    year_published: 1963
+            var deferred = $q.defer();
+            $timeout(function(){
+                if(getAllBooksCount % 4 == 3){
+                    deferred.reject("Error retrieving books.");
+                } else {
+                    deferred.notify("Please wait patiently....");
+                    $timeout(function(){
+                        deferred.resolve(booksArray);
+                    }, 1000);
                 }
-            ];
+                getAllBooksCount++;
+            }, 1000);
+
+            return deferred.promise;
         }
 
         function getAllReaders(){
 
             logger.output("Getting all readers.");
-            return [
-                {
-                    reader_id: 1,
-                    name: "Marie",
-                    weeklyReadingGoal: 315,
-                    totalMinutesRead: 5600
-                },
-                {
-                    reader_id: 2,
-                    name: "Daniel",
-                    weeklyReadingGoal: 210,
-                    totalMinutesRead: 3000
-                },
-                {
-                    reader_id: 3,
-                    name: "Lanier",
-                    weeklyReadingGoal: 140,
-                    totalMinutesRead: 600
-                }
+            var deferred = $q.defer();
+            $timeout(function(){
+                deferred.resolve(readersArray);
+            }, 1000);
 
-            ];
+            return deferred.promise;
         }
     }
 
-    dataServiceFactory.$inject = ["logger"];
+    dataServiceFactory.$inject = ["logger", "$q", "$timeout"];
 }());
